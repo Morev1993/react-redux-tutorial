fc104a6154dd2ad19d7a7f7d5619ccb41fe79265
Igor Morev <morev.i@mail.ru>
2016-04-27 17:46:33 +1000

    Добавил hot reloading, babel и react

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
     'webpack-hot-middleware/client',
+    'babel-polyfill',
     './src/index'
   ],
   output: {
@@ -14,7 +15,18 @@ module.exports = {
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ]
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: { //Обновлено
+    loaders: [ //добавили babel-loader
+      {
+        loaders: ['react-hot', 'babel-loader'],
+        include: [
+          path.resolve(__dirname, "src"),
+        ],
+        test: /\.js$/,
+        plugins: ['transform-runtime'],
+      }
+    ]
+  }
 }
